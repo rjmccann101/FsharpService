607cc2eeecb26d7a3077606fa48f6cfd5714ab2c
Richard Jackson McCann <52606032+rjmccann101@users.noreply.github.com>
2019-12-08 07:44:02 +0000

    Updated path to Dataflow asembly

--- TPLWorkflow.fsx
+++ TPLWorkflow.fsx
@@ -1,7 +1,7 @@
 ﻿// Script for TPL part of solution
 // dotnet add package System.Threading.Tasks.Dataflow
 
-#r "C:\\Users\\rjmcc\\.nuget\\packages\\system.threading.tasks.dataflow\\4.10.0\\lib\\netstandard2.0\\System.Threading.Tasks.Dataflow.dll"
+#r "C:\\Users\\rjmcc\\.nuget\\packages\\system.threading.tasks.dataflow\\4.11.0\\lib\\netstandard2.0\\System.Threading.Tasks.Dataflow.dll"
 open System.Threading.Tasks.Dataflow
 
 // Each TPL transform block takes an object of type 'a and returns object(s) of type 'b
