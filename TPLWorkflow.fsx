671115dac8ed9f5ba90de99cbb544d514453b5d7
Richard Jackson McCann <52606032+rjmccann101@users.noreply.github.com>
2019-12-08 07:24:47 +0000

    Updated to .NET Core 3.1 and latest versions of Nuget packages.

---
+++ TPLWorkflow.fsx
@@ -0,0 +1,15 @@
+﻿// Script for TPL part of solution
+// dotnet add package System.Threading.Tasks.Dataflow
+
+#r "C:\\Users\\rjmcc\\.nuget\\packages\\system.threading.tasks.dataflow\\4.10.0\\lib\\netstandard2.0\\System.Threading.Tasks.Dataflow.dll"
+open System.Threading.Tasks.Dataflow
+
+// Each TPL transform block takes an object of type 'a and returns object(s) of type 'b
+// An action block takes an object of type 'a
+// Here the objects that will be passed into the transform blocks and action blocks are
+// defined.
+
+// Object that describes a newly created file
+type ZipFile = {
+    fileName : string
+}
